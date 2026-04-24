fad8c7b925588ca66867dcf6b4aca6dce7029144
Mahfuz <mahfuzru5634@gmail.com>
2026-04-24 19:09:44 +0600

    fixed error

--- Server/server.js
+++ Server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { ObjectId } = require("mongodb");
-const { connectToServer, getDb } = require("../DB connection/db");
+const { connectToServer, getDb } = require("../DB_connection/db");
 const {
   verifyToken,
   verifySystemAdmin,
@@ -520,7 +520,14 @@ app.get('/', (req, res) => {
   res.send('Alumni Directory Backend Server is Running Perfectly!');
 });
 
-//const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
+connectToServer().catch(console.error);
 
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
 
+module.exports = app;
