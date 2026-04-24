faea33bd257c0bc6e8012bf44b120bae0088f75a
Mahfuz <mahfuzru5634@gmail.com>
2026-04-24 21:17:21 +0600

    fixed cors error

--- Server/server.js
+++ Server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: [
-    'https://universal-alumni-directory-frontend.vercel.app',
+    'https://universal-alumni-directory.vercel.app',
     'http://localhost:5173'
   ],
   credentials: true,
